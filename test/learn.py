c98240de26464d1ddada43ac2a926f2296fada1d
MaxianEdison <maximilianedison@gmail.com>
2022-04-02 18:56:05 +0430

    tkinter

--- test/learn.py
+++ test/learn.py
@@ -1,3 +1,22 @@
-for i in range(3):
-    w = int(input('Enter a number : '))
-    print('[%*s]' % (w, 'foo'))
+from tkinter import *
+
+
+
+root = Tk()
+root.title('simple app')
+root.geometry('500x400')
+
+txt = Label(root, text = 'this is example text')
+txt.grid()
+
+txtt = Entry(root, width = 10)
+txtt.grid(column = 1, row = 0)
+
+def clicked():
+    res = 'You are ' + txtt.get()
+    txt.configure(text = res)
+    
+btn = Button(root, text = 'Click me!', fg = 'red', command = clicked)
+btn.grid(column = 2, row = 0)
+
+root.mainloop()
