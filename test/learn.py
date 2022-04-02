6ff70a6af11ed0271826d934ba5ec201a087f242
MaxianEdison <maximilianedison@gmail.com>
2022-04-02 20:27:38 +0430

    make basic frame

--- test/learn.py
+++ test/learn.py
@@ -1,22 +1,27 @@
-from tkinter import *
-
-
+from tkinter import * 
 
 root = Tk()
-root.title('simple app')
+
+root.title('title page')
 root.geometry('500x400')
 
-txt = Label(root, text = 'this is example text')
-txt.grid()
+menu = Menu(root)
+item = Menu(menu)
+item.add_command(label = 'New')
+menu.add_cascade(label = 'File', menu = item)
+root.config(menu = menu)
 
-txtt = Entry(root, width = 10)
-txtt.grid(column = 1, row = 0)
+lbl = Label(root, text = 'this is some text')
+lbl.grid(column = 0, row = 0)
+
+txt = Entry(root, width = 20)
+txt.grid(column = 1, row = 0)
 
 def clicked():
-    res = 'You are ' + txtt.get()
-    txt.configure(text = res)
-    
-btn = Button(root, text = 'Click me!', fg = 'red', command = clicked)
+    res = 'welcome ' + txt.get()
+    lbl.configure(text = '%s' % res)
+
+btn = Button(root, text = 'click me', fg = 'blue', command = clicked)
 btn.grid(column = 2, row = 0)
 
 root.mainloop()
